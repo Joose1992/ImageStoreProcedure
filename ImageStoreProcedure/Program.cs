a420b68c77b566c44ac5f017ea67304acfa192f7
Jose Montanez <josemontanez1992@gmail.com>
2024-01-04 13:47:13 -0700

    Add project files.

---
+++ ImageStoreProcedure/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Net.Security;
+
+namespace ImageStoreProcedure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\josem\Pictures\Screenshots\Screenshot 2023-09-28 121500.png";
+            StoreImage store = new StoreImage(path);
+            store.UploadImage();
+        }
+
+        
+
+        
+    }
+}
---
+++ ImageStoreProcedure/StoreImage.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace ImageStoreProcedure
+{
+    public class StoreImage
+    {
+        private readonly string connectionString = "Server=lenovo_Jose;Database=PracticeDb;Trusted_Connection=True;";
+        private string imagePath;
+        public StoreImage(string path)
+        {
+            imagePath = path;
+        }
+
+        public void UploadImage()
+        {
+            byte[] imageData = ReadImageFile(imagePath);
+            if (imageData != null)
+            {
+                InsertImageIntoDatabase(connectionString, imageData);
+                Console.WriteLine("Image inserted into database!");
+            }
+            else
+            {
+                Console.WriteLine("Error reading image file");
+            }
+        }
+        private byte[] ReadImageFile(string imagePath)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(imagePath, FileMode.Open, FileAccess.Read))
+                {
+                    using (BinaryReader br = new BinaryReader(fs))
+                    {
+                        return br.ReadBytes((int)fs.Length);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading image file: {ex.Message}");
+                return null;
+            }
+        }
+        private void InsertImageIntoDatabase(string connectionString, byte[] imageData)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string insertQuery = "INSERT INTO IMAGES (ImageData) VALUES (@ImageData)";
+
+                    using (SqlCommand cmd = new SqlCommand(insertQuery, connection))
+                    {
+                        cmd.Parameters.Add("@ImageData", SqlDbType.VarBinary, -1).Value = imageData;
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error inserting image into database: {ex.Message}");
+            }
+        }
+    }
+}
